5ab83d8797cdc9ca9b72d297e9b0e8b9fc0e300a
zxj <775718540@qq.com>
2024-11-26 10:52:55 +0800

    feat(demo): 更新端口并添加文件上传功能
    - 将服务器端口从 8081 更改为 9312
    - 增加了文件上传相关的配置和处理逻辑
    - 添加了 fastjson2 依赖用于 JSON 处理

--- demo/src/main/java/org/kubo/demo/controller/GreetingController.java
+++ demo/src/main/java/org/kubo/demo/controller/GreetingController.java
@@ -1,6 +1,9 @@
 package org.kubo.demo.controller;
 
+import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
@@ -17,9 +20,13 @@ public class GreetingController {
         return Mono.just("ok");
     }
 
-    @PostMapping(value = "/{id}/status/detect")
-    public Mono<String> addPtzStatusByFile(@PathVariable(value = "id") String id, @RequestPart(value = "file", required = false) List<MultipartFile> file,
-                                                   @RequestPart(value = "desc", required = false) String desc, @RequestPart(value = "status", required = false) String status) {
-        return Mono.just("success");
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, value = "/ptz/{id}/status/detect")
+    public Mono<JSONObject> addPtzStatusByFile(@PathVariable(value = "id") String id, @RequestPart(value = "file", required = false) List<FilePart> file,
+                                               @RequestPart(value = "desc", required = false) String desc, @RequestPart(value = "status", required = false) String status) {
+        log.info(status);
+        log.info("status");
+        JSONObject data = new JSONObject();
+        data.put("res","success");
+        return Mono.just(data);
     }
 }
