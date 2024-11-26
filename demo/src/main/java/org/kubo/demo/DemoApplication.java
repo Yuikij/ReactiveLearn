44cbeed6ec4300842ac76cb50d03f51a3c6a6f81
zxj <775718540@qq.com>
2024-11-26 09:30:48 +0800

    测试servlet和webflux共存

--- demo/src/main/java/org/kubo/demo/DemoApplication.java
+++ demo/src/main/java/org/kubo/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package org.kubo.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import reactor.core.Disposable;
@@ -12,18 +13,10 @@ import java.util.function.Consumer;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-        GreetingClient greetingClient = context.getBean(GreetingClient.class);
-        Greeting greeting = new Greeting();
-        greeting.setMessage("1");
-        // We need to block for the content here or the JVM might exit before the message is logged
-        Mono<String> message1 = greetingClient.getMessage();
-        Mono<String> messageSouKon = greetingClient.getMessage();
-        Disposable subscribe = messageSouKon.subscribe((e) -> {
-            System.out.println(greeting.getMessage() + ":" + Thread.currentThread().getName());
-        });
-        System.out.println("end get:" + Thread.currentThread().getName());
-        greeting.setMessage("123");
+       SpringApplication.run(DemoApplication.class, args);
+//        SpringApplication app = new SpringApplication(DemoApplication.class);
+//        app.setWebApplicationType(WebApplicationType.REACTIVE);
+//        app.run( args);
     }
 
 }
--- demo/src/main/java/org/kubo/demo/controller/GreetingController.java
+++ demo/src/main/java/org/kubo/demo/controller/GreetingController.java
@@ -1,7 +1,25 @@
 package org.kubo.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
+@Slf4j
 public class GreetingController {
+
+    @PostMapping(value = "/111")
+    public Mono<Object> updateMultiPtzStatus() {
+        log.info("ok");
+        return Mono.just("ok");
+    }
+
+    @PostMapping(value = "/{id}/status/detect")
+    public Mono<String> addPtzStatusByFile(@PathVariable(value = "id") String id, @RequestPart(value = "file", required = false) List<MultipartFile> file,
+                                                   @RequestPart(value = "desc", required = false) String desc, @RequestPart(value = "status", required = false) String status) {
+        return Mono.just("success");
+    }
 }
